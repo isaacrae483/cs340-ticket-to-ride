f141d61732749bb054f6ba4018435aca79ae6917
Isaac <irae483@gmail.com>
2019-01-30 17:54:08 -0700

    updated some getters and setters

--- tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Commands/IncrementPlayersCommand.java
+++ tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Commands/IncrementPlayersCommand.java
@@ -1,10 +1,24 @@
 package edu.byu.cs340.tickettoride.shared.Commands;
 
+//import edu.byu.cs340.tickettoride.client.ClientFacade;
+import edu.byu.cs340.tickettoride.shared.Game.ID;
 import edu.byu.cs340.tickettoride.shared.Interface.ICommand;
+import edu.byu.cs340.tickettoride.shared.User.Username;
 
 public class IncrementPlayersCommand implements ICommand {
+    ID gameID;
+    Username newUser;
     @Override
     public Object execute() {
+        //ClientFacade.instance().incrementPlayers(gameID, newUser);
         return null;
+    }
+
+    public Username getNewUser() {
+        return newUser;
+    }
+
+    public ID getGameID() {
+        return gameID;
     }
 }
--- tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Commands/NewGameCommand.java
+++ tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Commands/NewGameCommand.java
@@ -1,11 +1,17 @@
 package edu.byu.cs340.tickettoride.shared.Commands;
 
+//import edu.byu.cs340.tickettoride.client.ClientModel;
+import edu.byu.cs340.tickettoride.shared.Game.Game;
 import edu.byu.cs340.tickettoride.shared.Interface.ICommand;
 
 public class NewGameCommand implements ICommand {
     //sent from the server to the client to update list
+    private Game game;
     @Override
     public Object execute() {
+        //ClientModel.instance().addGame(game);
         return null;
     }
+
+
 }
