df433b80e5abf06feb296af5d71fc0c3cd8641e5
Isaac <irae483@gmail.com>
2019-02-01 12:38:54 -0700

    moved commands

---
+++ app/src/main/java/edu/byu/cs340/tickettoride/Client/ClientCommands/NewGameCommand.java
@@ -0,0 +1,21 @@
+package edu.byu.cs340.tickettoride.Client.ClientCommands;
+
+import edu.byu.cs340.tickettoride.Client.ClientFacade;
+import edu.byu.cs340.tickettoride.shared.Game.Game;
+import edu.byu.cs340.tickettoride.shared.Interface.ICommand;
+
+public class NewGameCommand implements ICommand {
+    //sent from the server to the client to update list
+    public NewGameCommand(String username) {
+        this.username = username;
+    }
+
+    private String username;
+
+    private Game game;
+    @Override
+    public Object execute() {
+        ClientFacade.instance().addGame(game);
+        return null;
+    }
+}
