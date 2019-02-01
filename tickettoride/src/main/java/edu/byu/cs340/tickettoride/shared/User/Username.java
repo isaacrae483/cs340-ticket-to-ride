93f8bca8a2e0532cc5ec90acf7e665e081f763fe
bswhe_000 <bswheel@msn.com>
2019-01-31 20:45:58 -0700

    User class members verification methods

--- tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/User/Username.java
+++ tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/User/Username.java
@@ -4,10 +4,34 @@ public class Username{
     private String username;
 
     public Username(String username) {
-        this.username = username;
+        try {
+            qualifyUsername(username);
+            this.username = username;
+        }catch(Throwable t){
+            t.printStackTrace();
+        }
     }
 
     public String getUsername() {
         return username;
+    }
+
+    private class InvalidUserNameException extends Throwable{
+        // Parameterless Constructor
+        public InvalidUserNameException() {}
+
+        // Constructor that accepts a message
+        private InvalidUserNameException(String message)
+        {
+            super(message);
+        }
+    }
+
+    private void qualifyUsername(String username) throws InvalidUserNameException{
+        username = username.toLowerCase();
+        if(!username.matches("[a-z0-9!@#$%^&*()][a-z0-9!@#$%^&*()]*")){
+            throw new InvalidUserNameException("The username you have entered is invalid.");
+        }
+
     }
 }
