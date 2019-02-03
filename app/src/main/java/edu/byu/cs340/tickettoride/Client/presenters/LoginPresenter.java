4f0e42675cb4ca5bba9e3d8ae174bf5625887c4a
Isaac <irae483@gmail.com>
2019-02-02 18:18:51 -0700

    most of the implementation

--- app/src/main/java/edu/byu/cs340/tickettoride/Client/presenters/LoginPresenter.java
+++ app/src/main/java/edu/byu/cs340/tickettoride/Client/presenters/LoginPresenter.java
@@ -9,14 +9,39 @@ import edu.byu.cs340.tickettoride.shared.User.User;
 import edu.byu.cs340.tickettoride.shared.User.Username;
 
 public class LoginPresenter {
+        Username username;
+        Password password;
 
-
-    public void login(Username username, Password password){
-
+    public void login(String username_string, String password_string){
+        try{
+            username = new Username(username_string);
+        }catch(Exception e){
+            displayToast("Invalid Username");
+        }
+        try{
+            password = new Password(password_string);
+        }catch(Exception e){
+            displayToast("Invalid Password");
+        }
+        User user = new User(username, password);
+        LoginTask task = new LoginTask();
+        task.execute(user);
     }
 
-    public void register(Username username, Password password){
-
+    public void register(String username_string, String password_string){
+        try{
+            username = new Username(username_string);
+        }catch(Exception e){
+            displayToast("Invalid Username");
+        }
+        try{
+            password = new Password(password_string);
+        }catch(Exception e){
+            displayToast("Invalid Password");
+        }
+        User user = new User(username, password);
+        RegisterTask task = new RegisterTask();
+        task.execute(user);
     }
 
     public void update(){
@@ -39,6 +64,21 @@ public class LoginPresenter {
             password = user.getPassword();
 
             ClientFacade.instance().login(username, password);
+            return true;
+        }
+    }
+
+    public class RegisterTask extends AsyncTask<User, Integer, Boolean> {
+        User user;
+        Username username;
+        Password password;
+        @Override
+        protected Boolean doInBackground(User...users){
+            user = users[0];
+            username = user.getUserName();
+            password = user.getPassword();
+
+            ClientFacade.instance().register(username, password);
             return true;
         }
     }
