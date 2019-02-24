dd0a4b942731d4d3b2d1afb5dd58f967a66686d0
Thomas Lewis <thomasbartlewis@gmail.com>
2019-02-24 16:23:23 -0700

    Map Fragment class stub and layout stub created.

---
+++ app/src/main/java/edu/byu/cs340/tickettoride/Client/views/MapFragment.java
@@ -0,0 +1,28 @@
+package edu.byu.cs340.tickettoride.Client.views;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Thomas Lewis on 2/24/19.
+ */
+public class MapFragment extends PresenterViewFragment {
+
+    private View mFragmentView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //mFragmentView = inflater.inflate()
+
+        //return inflater.inflate(,container, false);
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        return null;
+    }
+
+
+}
