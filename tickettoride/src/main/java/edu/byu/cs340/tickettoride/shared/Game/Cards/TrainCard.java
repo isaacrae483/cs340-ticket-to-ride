94cdc2eb279340535a9fc0758f3387a123a1cfe8
James Ashdown <james.ashdown@byu.net>
2019-02-23 21:37:16 -0700

    added TrainCardDeck, modified TrainCard to parameterize constructor with color

--- tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Game/Cards/TrainCard.java
+++ tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Game/Cards/TrainCard.java
@@ -5,6 +5,10 @@ import edu.byu.cs340.tickettoride.shared.Game.Colors;
 public class TrainCard {
     private Colors color;
 
+    public TrainCard(Colors color) {
+        this.color = color;
+    }
+
     public Colors getColor() {
         return color;
     }
