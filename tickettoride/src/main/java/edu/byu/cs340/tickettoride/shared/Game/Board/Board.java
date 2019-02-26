d0af18de4325f5a16238507e221d1786f7e33fe5
bswhe_000 <bswheel@msn.com>
2019-02-25 21:26:21 -0700

    added Board Class and Route methods

--- tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Game/Board/Board.java
+++ tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Game/Board/Board.java
@@ -1,4 +1,25 @@
 package edu.byu.cs340.tickettoride.shared.Game.Board;
 
+import edu.byu.cs340.tickettoride.shared.User.Username;
+
 public class Board {
+    Routes routes;
+
+    public Board(Routes routes) {
+        this.routes = routes;
+    }
+
+    public void claimRoute(Route route, Username username){
+        route.claimRoute(username);
+    }
+
+
+    public Routes getRoutes() {
+        return routes;
+    }
+
+    public Routes getPlayerRoutes(Username username) {
+        //return Routes.getRoutes(username);
+        return null;
+    }
 }
