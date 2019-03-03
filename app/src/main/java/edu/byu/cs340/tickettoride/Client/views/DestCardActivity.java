6c557768189131feea8b20d70e33555a16ed4887
green_admin <green.avery.s@gmail.com>
2019-03-02 18:51:05 -0700

    DestCard view

---
+++ app/src/main/java/edu/byu/cs340/tickettoride/Client/views/DestCardActivity.java
@@ -0,0 +1,22 @@
+package edu.byu.cs340.tickettoride.Client.views;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import edu.byu.cs340.tickettoride.R;
+
+public class DestCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dest_card);
+
+
+    }
+
+}
