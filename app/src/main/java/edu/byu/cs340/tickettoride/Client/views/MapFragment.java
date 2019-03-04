4e0ee5851ff4c7089c34c1e727698cbf92d17186
Thomas Lewis <thomasbartlewis@gmail.com>
2019-03-03 23:58:54 -0700

    MapFragment is now zoomable, MapFragment will stay a fixed size for any screen to prevent image drift.

--- app/src/main/java/edu/byu/cs340/tickettoride/Client/views/MapFragment.java
+++ app/src/main/java/edu/byu/cs340/tickettoride/Client/views/MapFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import edu.byu.cs340.tickettoride.R;
 
@@ -15,11 +16,24 @@ import edu.byu.cs340.tickettoride.R;
 public class MapFragment extends PresenterViewFragment {
 
     private View mFragmentView;
+    private ImageView testView;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mFragmentView = inflater.inflate(R.layout.fragment_game_map, container, false);
+        testView = mFragmentView.findViewById(R.id.imageView6);
+        testView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeToast("View pressed");
+                //testView.setBackgroundColor();
+                testView.setColorFilter(getResources().getColor(R.color.greenPlayer));
+            }
+        });
+
+
 
         return mFragmentView;
         //return super.onCreateView(inflater, container, savedInstanceState);
