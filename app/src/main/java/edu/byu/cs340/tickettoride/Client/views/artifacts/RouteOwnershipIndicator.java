ae017fd605d9906fa4b8eb7444b1b52784d5823f
Thomas Lewis <thomasbartlewis@gmail.com>
2019-03-04 12:41:49 -0700

    Zoomable view group and rhombus resources

---
+++ app/src/main/java/edu/byu/cs340/tickettoride/Client/views/artifacts/RouteOwnershipIndicator.java
@@ -0,0 +1,58 @@
+package edu.byu.cs340.tickettoride.Client.views.artifacts;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import edu.byu.cs340.tickettoride.R;
+import edu.byu.cs340.tickettoride.shared.Interface.IPlayer;
+
+/**
+ * Created by Thomas Lewis on 3/3/19.
+ */
+public class RouteOwnershipIndicator extends AppCompatImageView implements IRouterOwnershipIndicator {
+
+
+    public RouteOwnershipIndicator(Context context) {
+        super(context);
+        init();
+    }
+
+    public RouteOwnershipIndicator(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public RouteOwnershipIndicator(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @Override
+    public void setColor(IPlayer.Color color) {
+        switch (color) {
+            case BLACK:
+                this.setBackgroundResource(R.drawable.ic_rhomb_outline);
+                break;
+            case BLUE:
+                this.setBackgroundResource(R.drawable.ic_rhomb_outline);
+                break;
+            case RED:
+                this.setBackgroundResource(R.drawable.ic_rhomb_outline);
+                break;
+            case GREEN:
+                this.setBackgroundResource(R.drawable.ic_rhomb_outline);
+                break;
+            case YELLOW:
+                this.setBackgroundResource(R.drawable.ic_rhomb_outline);
+                break;
+                default:
+                    this.setBackgroundResource(R.drawable.ic_rhomb_outline);
+        }
+    }
+
+    private void init() {
+        this.setBackgroundResource(R.drawable.ic_rhomb_outline);
+    }
+}
