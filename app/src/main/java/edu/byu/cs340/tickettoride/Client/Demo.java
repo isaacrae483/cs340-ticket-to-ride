ee0e8c93a84f4d29cb650a4b90e2aa7f96a04361
irae4 <irae483@gmail.com>
2019-03-08 20:31:55 -0700

    empty demo class

---
+++ app/src/main/java/edu/byu/cs340/tickettoride/Client/Demo.java
@@ -0,0 +1,20 @@
+package edu.byu.cs340.tickettoride.Client;
+
+public class Demo {
+    /*
+    Update player points
+● Add/remove train cards for this player
+● Add/remove player destination cards for this player
+● Update the number of train cards for opponent players
+● Update the number of train cars for opponent players
+● Update the number of destination cards for opponent players
+● Update the visible (face up) cards in the train card deck
+● Update the number of invisible (face down) cards in train card deck
+● Update the number of cards in destination card deck
+● Add claimed route (for any player). Show this on the map.
+● Add chat message from any player
+● Advance player turn (change the turn indicator so it indicates another
+player)
+
+     */
+}
