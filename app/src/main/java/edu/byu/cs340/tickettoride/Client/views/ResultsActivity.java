08fe1282b0841a544fd52ab2ff4a6ed5af7ba9cc
irae4 <irae483@gmail.com>
2019-03-13 18:56:14 -0600

    Result view xml

---
+++ app/src/main/java/edu/byu/cs340/tickettoride/Client/views/ResultsActivity.java
@@ -0,0 +1,15 @@
+package edu.byu.cs340.tickettoride.Client.views;
+
+import android.support.v7.app.AppCompatActivity;
+import edu.byu.cs340.tickettoride.R;
+
+import android.os.Bundle;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+}
