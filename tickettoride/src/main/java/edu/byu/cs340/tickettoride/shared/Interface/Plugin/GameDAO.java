9952eb80ea8636d36789c05912bbe3e6364de0c2
bswhe_000 <bswheel@msn.com>
2019-04-10 12:31:55 -0600

    Game DAO Interface

--- tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Interface/Plugin/GameDAO.java
+++ tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Interface/Plugin/GameDAO.java
@@ -6,6 +6,6 @@ import edu.byu.cs340.tickettoride.shared.Game.Game;
 public interface GameDAO {
     void updateGame(ServerCommandData data);
     void deleteGame();
-    //static MapGameDAO getGames();
+    MapGameDAO getGames();
     Game getGame();
 }
