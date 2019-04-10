0779cb868c9d139a9d57dead8c012168862ad13b
irae4 <irae483@gmail.com>
2019-04-10 12:17:03 -0600

    complete Interface

--- tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Interface/Plugin/UserDAO.java
+++ tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Interface/Plugin/UserDAO.java
@@ -1,5 +1,14 @@
 package edu.byu.cs340.tickettoride.shared.Interface.Plugin;
 
-public interface UserDAO {
+import java.util.Map;
 
+import edu.byu.cs340.tickettoride.server.Model.CommandList;
+import edu.byu.cs340.tickettoride.shared.User.MapUsers;
+import edu.byu.cs340.tickettoride.shared.User.Password;
+import edu.byu.cs340.tickettoride.shared.User.Username;
+
+public interface UserDAO {
+    void register(Username username, Password password);
+    MapUsers get();
+    void updateCommandQueue(CommandList commandList);
 }
