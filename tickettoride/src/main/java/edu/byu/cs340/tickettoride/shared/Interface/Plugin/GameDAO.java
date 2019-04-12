e29e97851cfba3151c9dc14a9c202b7b4d0690d8
bswhe_000 <bswheel@msn.com>
2019-04-11 19:54:09 -0600

    added SQLGameDAO Methods

--- tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Interface/Plugin/GameDAO.java
+++ tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Interface/Plugin/GameDAO.java
@@ -1,9 +1,11 @@
 package edu.byu.cs340.tickettoride.shared.Interface.Plugin;
 
 
+import java.util.List;
+
 public interface GameDAO {
     void updateGame(String data, String game);
     void deleteGame();
     String getGame();
-    String getCommands();
+    List<String> getCommands();
 }
--- tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Interface/Plugin/SQLPlugin/SQLGameDAO.java
+++ tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Interface/Plugin/SQLPlugin/SQLGameDAO.java
@@ -1,46 +1,197 @@
 package edu.byu.cs340.tickettoride.shared.Interface.Plugin.SQLPlugin;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.transform.Result;
 
 import edu.byu.cs340.tickettoride.shared.Interface.Plugin.GameDAO;
 import edu.byu.cs340.tickettoride.shared.Interface.Plugin.MapGameDAO;
 
 public class SQLGameDAO implements GameDAO {
+    // ----- DataBase Initialization ------
+    Connection conn;
+    SQLGameDAO(Connection connection){
+        createTable(connection);
+        conn = connection;
+    }
+
+    public void createTable(Connection connection){
+        Statement stmt = null;
+        try {
+            try {
+                stmt = connection.createStatement();
+                stmt.executeUpdate("DROP TABLE IF EXISTS Game");
+                stmt.executeUpdate("DROP TABLE IF EXISTS Delta");
+                stmt.executeUpdate("CREATE TABLE 'Delta' (" +
+                        //(note: there should be multiple for each game at a time)
+                        "ID TEXT NOT NULL," +
+                        "Data TEXT NOT NULL,"+
+                        ")");
+                stmt.executeUpdate("CREATE TABLE 'Game' (" +
+                        "ID TEXT NOT NULL UNIQUE," +
+                        "Game TEXT NOT NULL,"+
+                        ")");
+
+            }finally {
+                if(stmt != null){
+                    stmt.close();
+                    stmt = null;
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    // ----- GameDAO Operations -----
     @Override
+
     public void updateGame(String data, String game) {
         //if not at a checkpoint (numTillCheckpoint > 0)
-            //save data as the delta (it will be of type serverCommandData)
-            //in the table, the first column will be ID and the second column will be the data
-            //(note: there should be multiple for each game at a time)
+        if(numTillCheckpoint > 0){
+            //save data as the delta ?(it will be of type serverCommandData)? *
+            try{
+                PreparedStatement stmt = null;
+                //in the table, the first column will be ID and the second column will be the data
+                try{
+                    String sql = "insert into Delta values (?,?)";
+                    stmt = conn.prepareStatement(sql);
+                    stmt.setString(1, this.id);
+                    stmt.setString(2, data);
+                    if(stmt.executeUpdate() != 1){
+                        throw new Exception ("Could Not Save Delta");
+                    }
+                }finally {
+                    if (stmt != null) {
+                        stmt.close();
+                        stmt = null;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             //subtract one from numTillCheckpoint
-        //else
+            numTillCheckpoint--;
+        } else {
             //save game
-            //in the game table, save the game with the first column as ID and the second column as game
-            //delete all commands from the delta table for that game
-            //set numTillCheckpoint = 0
+            try{
+                PreparedStatement stmt = null;
+                //in the game table, save the game with the first column as ID and the second column as game
+                try{
+                    String sql = "insert int Game values (?,?)";
+                    stmt = conn.prepareStatement(sql);
+                    stmt.setString(1, this.id);
+                    stmt.setString(2, game);
+                    if(stmt.executeUpdate() != 1){
+                        throw new Exception ("Could Not Save Game");
+                    } else {
+                        //delete all commands from the delta table for that game
+                        sql = "delete from Delta where ID = ?";
+                        stmt = conn.prepareStatement(sql);
+                        stmt.setString(1, game);
+                        if(stmt.executeUpdate() != 1){
+                            throw new Exception ("Could Not Delete Deltas");
+                        }
+                    }
+                }finally {
+                    if (stmt != null) {
+                        stmt.close();
+                        stmt = null;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            //set numTillCheckpoint = 0 (do you mean back to this.deltas?)
+            numTillCheckpoint = this.deltas;
+        }
     }
 
 
     @Override
     public void deleteGame() {
         //delete rows from delta table and from game table where ID = id
+        try{
+            PreparedStatement stmt = null;
+            try{
+                String sql = "delete from Game where ID = ?";
+                stmt = conn.prepareStatement(sql);
+                stmt.setString(1, id);
+                if(stmt.executeUpdate() != 1){
+                    throw new Exception ("Could Not Delete Game");
+                }
+            }finally {
+                if (stmt != null) {
+                    stmt.close();
+                    stmt = null;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public String getGame() {
         //return game where ID = id
-        return null;
+        String game = null; // what type should this have?
+        try{
+            String sql = "select from Game where ID = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, id);
+            ResultSet results = stmt.executeQuery();
+            while (results.next()) {
+                game = results.getString(2);
+            }
+            return game;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
-    public String getCommands() {
+    public List<String> getCommands() {
         //return commands in delta table where ID = id
-        return null;
+        List<String> commands = new ArrayList<>();
+        try{
+            String query = "select * from Delta where ID = ?";
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, this.id);
+            ResultSet results = stmt.executeQuery();
+            while (results.next()) {
+                commands.add(results.getString(2));
+            }
+            return commands;
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
+
+
+    // This cod needs help. How and why do we create a new GameDAO here?
 
     public static MapGameDAO getGames() {
         MapGameDAO games = new MapGameDAO();
-        //for each ID in game or deltas,
-            //create a new GameDAO with that ID
-            //add to games
+        /*try{
+            //for each ID in game or deltas,
+            String query = "select * from Delta UNION ALL " +
+                    "select * from Game";
+            PreparedStatement stmt = conn.prepareStatement(query);
+            ResultSet results = stmt.executeQuery();
+            while (results.next()) {
+                // create a new GameDAO with that ID
+                // add to games
+                games.add(results.getString(1), new SQLGameDAO(results.getString(1), results.getString(2));
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }*/
         return games;
     }
 
