48e37f02ae4a0b9177411d05213396a365a138ac
irae4 <irae483@gmail.com>
2019-04-17 15:26:01 -0600

    hello

--- tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Interface/Plugin/FlatFilePlugin/DAOFactory.java
+++
@@ -1,7 +0,0 @@
-package edu.byu.cs340.tickettoride.shared.Interface.Plugin.FlatFilePlugin;
-
-
-public interface DAOFactory {
-    UserDAO generateUserDAO();
-    GameDAO generateGameDAO(int deltas);
-}
--- tickettoride/src/main/java/edu/byu/cs340/tickettoride/shared/Interface/Plugin/SQLPlugin/DAOFactory.java
+++
@@ -1,7 +0,0 @@
-package edu.byu.cs340.tickettoride.shared.Interface.Plugin.SQLPlugin;
-
-
-public interface DAOFactory {
-    UserDAO generateUserDAO();
-    GameDAO generateGameDAO(int deltas);
-}
